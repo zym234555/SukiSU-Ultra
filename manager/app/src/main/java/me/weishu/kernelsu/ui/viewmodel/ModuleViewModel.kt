bb8c9ffac5d91b3794604fb4c40db359acefdb8b
5ec1cff <ewtqyqyewtqyqy@gmail.com>
2024-11-23 19:13:36 +0800

    manager: no need to check overlayfs

--- manager/app/src/main/java/me/weishu/kernelsu/ui/viewmodel/ModuleViewModel.kt
+++ manager/app/src/main/java/me/weishu/kernelsu/ui/viewmodel/ModuleViewModel.kt
@@ -13,7 +13,6 @@ import kotlinx.coroutines.launch
 import me.weishu.kernelsu.ksuApp
 import me.weishu.kernelsu.ui.util.HanziToPinyin
 import me.weishu.kernelsu.ui.util.listModules
-import me.weishu.kernelsu.ui.util.overlayFsAvailable
 import org.json.JSONArray
 import org.json.JSONObject
 import java.text.Collator
@@ -52,9 +51,6 @@ class ModuleViewModel : ViewModel() {
         private set
     var search by mutableStateOf("")
 
-    var isOverlayAvailable by mutableStateOf(overlayFsAvailable())
-        private set
-
     var sortEnabledFirst by mutableStateOf(false)
     var sortActionFirst by mutableStateOf(false)
     val moduleList by derivedStateOf {
@@ -87,8 +83,6 @@ class ModuleViewModel : ViewModel() {
             val start = SystemClock.elapsedRealtime()
 
             kotlin.runCatching {
-                isOverlayAvailable = overlayFsAvailable()
-
                 val result = listModules()
 
                 Log.i(TAG, "result: $result")
