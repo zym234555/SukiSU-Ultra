182028d9ea656ddefc0160e4d6afaacdeeaed6b4
ShirkNeko <109797057+ShirkNeko@users.noreply.github.com>
2025-04-14 19:06:54 +0800

    Updating the downloader to use the new GitHub publishing interface

--- manager/app/src/main/java/zako/zako/zako/ui/util/Downloader.kt
+++ manager/app/src/main/java/zako/zako/zako/ui/util/Downloader.kt
@@ -63,8 +63,7 @@ fun download(
 }
 
 fun checkNewVersion(): LatestVersionInfo {
-    // 改为新的 release 接口
-    val url = "https://api.github.com/repos/zako/SukiSU-Ultra/releases/latest"
+    val url = "https://api.github.com/repos/ShirkNeko/SukiSU-Ultra/releases/latest"
     val defaultValue = LatestVersionInfo()
     return runCatching {
         okhttp3.OkHttpClient().newCall(okhttp3.Request.Builder().url(url).build()).execute()
