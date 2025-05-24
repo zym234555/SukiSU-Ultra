d408c9f4bf074ea351ed288b8227bccd708ac9d0
WenHao2130 <98936399+WenHao2130@users.noreply.github.com>
2025-05-24 15:33:50 +0800

    manager: Modify Module page icon (#104)
    Signed-off-by: WenHao2130 <wenhao2130@outlook.com>

--- manager/app/src/main/java/com/sukisu/ultra/ui/screen/BottomBarDestination.kt
+++ manager/app/src/main/java/com/sukisu/ultra/ui/screen/BottomBarDestination.kt
@@ -23,6 +23,6 @@ enum class BottomBarDestination(
     Home(HomeScreenDestination, R.string.home, Icons.Filled.Home, Icons.Outlined.Home, false),
     Kpm(KpmScreenDestination, R.string.kpm_title, Icons.Filled.Build, Icons.Outlined.Build, true),
     SuperUser(SuperUserScreenDestination, R.string.superuser, Icons.Filled.AdminPanelSettings, Icons.Outlined.AdminPanelSettings, true),
-    Module(ModuleScreenDestination, R.string.module, Icons.Filled.Apps, Icons.Outlined.Apps, true),
+    Module(ModuleScreenDestination, R.string.module, Icons.Filled.Extension, Icons.Outlined.Extension, true),
     Settings(SettingScreenDestination, R.string.settings, Icons.Filled.Settings, Icons.Outlined.Settings, false),
 }
