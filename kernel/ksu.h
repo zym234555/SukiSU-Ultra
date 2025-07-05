5d48d9a923209bc8e16f20b2b8784071b9063ffd
ShirkNeko <109797057+ShirkNeko@users.noreply.github.com>
2025-07-05 15:56:19 +0800

    Add dynamic signature support

--- kernel/ksu.h
+++ kernel/ksu.h
@@ -29,6 +29,7 @@
 #define CMD_ENABLE_KPM 100
 #define CMD_HOOK_TYPE 101
 #define CMD_GET_SUSFS_FEATURE_STATUS 102
+#define CMD_DYNAMIC_SIGN 103
 
 #define EVENT_POST_FS_DATA 1
 #define EVENT_BOOT_COMPLETED 2
@@ -45,6 +46,16 @@
 #define KSU_VERSION_FULL "v3.x-00000000@unknown"
 #endif
 #define KSU_FULL_VERSION_STRING 255
+
+#define DYNAMIC_SIGN_OP_SET 0
+#define DYNAMIC_SIGN_OP_GET 1
+#define DYNAMIC_SIGN_OP_CLEAR 2
+
+struct dynamic_sign_user_config {
+    unsigned int operation;
+    unsigned int size;
+    char hash[65];
+};
 
 // SUSFS Functional State Structures
 struct susfs_feature_status {
