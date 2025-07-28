3ae1a3b10a82b6ff8e9e982c9e519d5faa11a797
ShirkNeko <109797057+ShirkNeko@users.noreply.github.com>
2025-07-28 22:44:24 +0800

    kermel: Refactoring and separating dynamic signature-related structures and functions

--- kernel/apk_sign.h
+++ kernel/apk_sign.h
@@ -3,32 +3,10 @@
 
 #include <linux/types.h>
 #include "ksu.h"
-#include "manager.h"
 
 bool is_manager_apk(char *path);
 
-struct dynamic_sign_config {
-    unsigned int size;
-    char hash[65];
-    int is_set;
-};
-
-struct manager_info {
-    uid_t uid;
-    int signature_index;
-    bool is_active;
-};
-
-bool ksu_is_multi_manager_apk(char *path, int *signature_index);
-void ksu_add_manager(uid_t uid, int signature_index);
-void ksu_remove_manager(uid_t uid);
-bool ksu_is_any_manager(uid_t uid);
-int ksu_get_manager_signature_index(uid_t uid);
-int ksu_get_active_managers(struct manager_list_info *info);
-
-int ksu_handle_dynamic_sign(struct dynamic_sign_user_config *config);
-void ksu_dynamic_sign_init(void);
-void ksu_dynamic_sign_exit(void);
-bool ksu_load_dynamic_sign(void);
+// Get dynamic sign configuration for signature verification
+bool ksu_get_dynamic_sign_config(unsigned int *size, const char **hash);
 
 #endif
