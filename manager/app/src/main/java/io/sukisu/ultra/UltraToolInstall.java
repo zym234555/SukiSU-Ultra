5586f9ce30a417eeffdaa737066604ce1e71564c
ShirkNeko <109797057+ShirkNeko@users.noreply.github.com>
2025-09-01 16:25:25 +0800

    manager: Optimize build configurations
    bump Gradle version and dep

--- manager/app/src/main/java/io/sukisu/ultra/UltraToolInstall.java
+++ manager/app/src/main/java/io/sukisu/ultra/UltraToolInstall.java
@@ -5,21 +5,25 @@ import com.topjohnwu.superuser.io.SuFile;
 import static com.sukisu.ultra.ui.util.KsuCliKt.getKpmmgrPath;
 import static com.sukisu.ultra.ui.util.KsuCliKt.getSuSFSDaemonPath;
 
+import android.annotation.SuppressLint;
+
 public class UltraToolInstall {
     private static final String OUTSIDE_KPMMGR_PATH = "/data/adb/ksu/bin/kpmmgr";
     private static final String OUTSIDE_SUSFSD_PATH = "/data/adb/ksu/bin/susfsd";
+    @SuppressLint("SetWorldReadable")
+    @SuppressWarnings("ResultOfMethodCallIgnored")
     public static void tryToInstall() {
         SuFile KpmmgrFile = new SuFile(OUTSIDE_KPMMGR_PATH);
         if (KpmmgrFile.exists()) {
             UltraShellHelper.CopyFileTo(getKpmmgrPath(), OUTSIDE_KPMMGR_PATH);
-            boolean _ = KpmmgrFile.setReadable(true, false);
-            boolean _ = KpmmgrFile.setExecutable(true, false);
+            KpmmgrFile.setReadable(true, false);
+            KpmmgrFile.setExecutable(true, false);
         }
         SuFile SuSFSDaemonFile = new SuFile(OUTSIDE_SUSFSD_PATH);
         if (SuSFSDaemonFile.exists()) {
             UltraShellHelper.CopyFileTo(getSuSFSDaemonPath(), OUTSIDE_SUSFSD_PATH);
-            boolean _ = SuSFSDaemonFile.setReadable(true, false);
-            boolean _ = SuSFSDaemonFile.setExecutable(true, false);
+            SuSFSDaemonFile.setReadable(true, false);
+            SuSFSDaemonFile.setExecutable(true, false);
         }
     }
 }
